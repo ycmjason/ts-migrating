cb78384b8eda96bcf6b9bcf3c4d9a48b3e0f4567
Jason Yu <me@ycmjason.com>
2025-09-06 07:16:21 +0100

    update example

--- examples/check-js-migration/src/index.js
+++ examples/check-js-migration/src/index.js
@@ -2,6 +2,7 @@ import { greet } from './greet';
 
 // @ts-migrating
 greet(true);
+//    ^ 👌 fallback to `checkJs: false`
 
-greet(1);
-//    ^ [ts-migrating] Argument of type 'number' is not assignable to parameter of type 'string'.
+greet(999999999999);
+//    ^ ❌ [ts-migrating] Argument of type 'number' is not assignable to parameter of type 'string'.
