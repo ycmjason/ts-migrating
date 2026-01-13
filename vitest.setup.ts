f896baa366dd99d80499707409017475076d19f7
YCM Jason <me@ycmjason.com>
2026-01-13 00:19:13 +0000

    housekeep (#10)

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,9 @@
 import { execSync } from 'node:child_process';
+import { rmSync } from 'node:fs';
+import { join } from 'node:path';
 import { beforeAll } from 'vitest';
 
 beforeAll(() => {
+  rmSync(join(__dirname, 'dist'), { recursive: true, force: true });
   execSync('pnpm build', { stdio: 'ignore' });
 });
